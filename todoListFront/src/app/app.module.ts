84ead22a9329ca50c671668c779731781b823ea4
Mircea Bardan <mircea.c.bardan@gmail.com>
2023-11-14 22:31:02 +0100

    clean-up

--- todoListFront/src/app/app.module.ts
+++ todoListFront/src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './component/todo-list/todo-list.component';
-import { TodoListService } from './service/todo-list.service';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
